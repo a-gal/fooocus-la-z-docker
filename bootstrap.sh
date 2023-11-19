ba16ba231579aeea018fa394f4a75a666aec6509
a-gal <52814981+a-gal@users.noreply.github.com>
2023-11-19 14:34:39 +0000

    give up

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,8 @@ repo_url="https://github.com/lllyasviel/Fooocus.git"
 
 if [ ! -e /app/fooocus/entry_with_update.py ]; then
     echo "Script file does not exist. Cloning..."
-    cd /app/fooocus && \
-    git init && \
-    git remote add origin "$repo_url" && \
-    git fetch && \
-    git reset origin/main && \
-    git checkout -t origin/main
-    if $? -ne 0; then
+    git clone "$repo_url" /app/fooocus
+    if ! git clone "$repo_url" /app/fooocus; then
         echo "Failed to clone repo"
         exit 1
     fi
@@ -18,7 +13,7 @@ fi
 
 if [ ! -e /app/venv ]; then
     echo "Venv does not exist. Creating..."
-    if ! venv /app/venv; then
+    if ! virtualenv /app/venv; then
         echo "Failed to venv"
         exit 1
     fi
